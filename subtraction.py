5a5c3c0ca7f7e6623006843cffd17cc561bf7d8a
Subdev514 <82996094+Subdev514@users.noreply.github.com>
2021-10-08 18:37:56 +0545

    added subraction

---
+++ subtraction.py
@@ -0,0 +1,4 @@
+def subtraction():
+    first_input = float(input('Enter your first number(the number to subtract): '))
+    second_input = float(input('Enter your second number(the number it should be subtracted by): '))
+    return first_input-second_input
