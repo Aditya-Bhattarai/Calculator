faf18caaa9adc25b23d67058554d3977ccafcebb
Aditya Bhattarai <adityabhattarai1424@gmail.com>
2021-10-08 18:44:40 +0545

    made the mian.py better

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
-
 from addition import addition
+from subtraction import subtract
 
+print('Welcome to one of the worst calculator app where we dont even have a gui!')
 
-addition()
+action = input('''
+
+What action would you like to perform?
+- Addition
+-Subtraction
+-multiplication
+-division
+-Power
+-Percentage
+
+> ''')
+
+if action == 'addition' or action == 'add':
+    addition()
+elif action == 'subtraction'.lower or action == 'subtract'.lower:
+    subtract()
+""" elif action == 'multiply'.lower or action == 'multiplication'.lower:
+    multiply()
+ """
--- subtraction.py
+++ subtraction.py
@@ -1,4 +1,6 @@
-def subtraction():
-    first_input = float(input('Enter your first number(the number to subtract): '))
-    second_input = float(input('Enter your second number(the number it should be subtracted by): '))
+def subtract():
+    first_input = float(
+        input('Enter your first number(the number to subtract): '))
+    second_input = float(
+        input('Enter your second number(the number it should be subtracted by): '))
     return first_input-second_input
